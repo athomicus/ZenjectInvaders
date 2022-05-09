05cd2dad221eb83346bbf69e17e3fb8afc03eee9
athomicus <96152125+athomicus@users.noreply.github.com>
2022-05-09 10:23:39 +0200

    Change Player

--- Assets/Scripts/Ship/Player.cs
+++ Assets/Scripts/Ship/Player.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
 
 public class Player : MonoBehaviour
 {
+    
+    [Inject]
+    public void Construct()
+    {
+
+    }
     //private float _speed = 5;
 
    /* public float GetSpeed()
